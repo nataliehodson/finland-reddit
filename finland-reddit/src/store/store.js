cab2d8eaf3ba6f445cba5bdb0e9c6c92df0552e6
Natalie <natalie.ac.hodson@gmail.com>
2022-08-07 23:12:08 +0300

    Add redditSliceReducer

--- finland-reddit/src/store/store.js
+++ finland-reddit/src/store/store.js
@@ -1,7 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import redditSliceReducer from './redditSlice';
 
 export default configureStore({
     reducer: combineReducers({
-        
+        redditSlice: redditSliceReducer,
     })
 })
