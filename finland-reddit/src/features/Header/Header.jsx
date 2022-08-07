a69cee4b0a92acb0908028b85915cd27be99f3e0
Natalie <natalie.ac.hodson@gmail.com>
2022-08-07 21:38:04 +0300

    Begin styling Header

--- finland-reddit/src/features/Header/Header.jsx
+++ finland-reddit/src/features/Header/Header.jsx
@@ -1,11 +1,18 @@
+import './Header.css';
+
 const Header = () => {
     return (
-        <header className="App-header">
-            <img src='../public/finland.png'/>
-            <h1>Finland on Reddit</h1>
-            <input placeholder='Search...'/>
-            {/*add search icon
-            add icon for reddit*/}
+        <header className="header">
+            <div className='title-image'>
+                <img src='../public/finland.png'/>
+                <h1>Finland on Reddit</h1>
+            </div>
+            <div className='search'>
+                <input placeholder='Search...'/>
+                {/*add search icon
+                add icon for reddit*/}
+            </div>
+            <a className='reddit' href='reddit.com'>Reddit</a>
         </header>
     )
 }
