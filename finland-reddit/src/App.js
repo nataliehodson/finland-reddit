77b6b80a04a8509cad041dcc3f3b18dfc5367ef8
Natalie <natalie.ac.hodson@gmail.com>
2022-08-07 21:18:54 +0300

    Add JSX to Header, add Header component to App.js

--- finland-reddit/src/App.js
+++ finland-reddit/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './features/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <body>
+
+      </body>
+      <footer>
+        <a href="https://www.flaticon.com/free-icons/finland" title="finland icons">Finland icons created by Anditii Creative - Flaticon</a>
+      </footer>
     </div>
   );
 }
---
+++ finland-reddit/src/features/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="App-header">
+            <img src='../public/finland.png'/>
+            <h1>Finland on Reddit</h1>
+            <input placeholder='Search...'/>
+            {/*add search icon
+            add icon for reddit*/}
+        </header>
+    )
+}
+
+export default Header;
