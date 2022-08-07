e29f35115db68187893e50c71d23841ce58f395a
Natalie <natalie.ac.hodson@gmail.com>
2022-08-07 23:26:34 +0300

    Create files for Post, write HTML to return

---
+++ finland-reddit/src/features/Post/Post.jsx
@@ -0,0 +1,25 @@
+import { getFinlandPosts } from "../../api/reddit";
+import { getSuomiPosts } from "../../api/reddit";
+
+const Post = () => {
+    return (
+        <div className="post">
+            <div className="sidebar">
+                <p>time posted</p>
+                <p>subreddit</p>
+                <p>username</p>
+                <div className="upvote-div">
+                    <svg className="icon"></svg>
+                    <p>nbr upvotes</p>
+                </div>
+            </div>
+            <div className="post-whole">
+                <h2>Post title</h2>
+                <img className="post-img" />
+                <div className="post-footer">
+                    <svg></svg>
+                </div>
+            </div>
+        </div>
+    )
+}
