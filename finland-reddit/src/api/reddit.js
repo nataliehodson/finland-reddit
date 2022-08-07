f7d08bb8b56a19b00cec5ed533862e5fbb61d9c4
Natalie <natalie.ac.hodson@gmail.com>
2022-08-07 23:32:00 +0300

    Fix spelling

--- finland-reddit/src/api/reddit.js
+++ finland-reddit/src/api/reddit.js
@@ -1,14 +1,14 @@
 export const apiRoot = 'https://www.reddit.com/search.json?q='
 
 export const getFinlandPosts = async () => {
-    const response = await fetch(`${rootApi}finland`);
+    const response = await fetch(`${apiRoot}finland`);
     const json = await response.json();
 
     return json.data.children.map((post) => post.data);
 };
 
 export const getSuomiPosts = async () => {
-    const response = await fetch(`${rootApi}suomi`);
+    const response = await fetch(`${apiRoot}suomi`);
     const json = await response.json();
 
     return json.data.children.map((post) => post.data);
